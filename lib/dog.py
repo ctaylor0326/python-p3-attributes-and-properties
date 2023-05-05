a22e4ec4d4a12367ba497fd2854dd3952fd9a07d
CahenTaylor <cahentaylor@gmail.com>
2023-05-05 09:39:30 -0600

    passed tests

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import ipdb
 
 APPROVED_BREEDS = [
     "Mastiff",
@@ -11,5 +12,37 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
+
 class Dog:
-    pass
+    def __init__(self, name="Pup", breed="Pointer"):
+        self.name = name
+        self.breed = breed
+
+    def get_name(self):
+        print(f"{self._name}")
+        return self._name
+
+    def set_name(self, name):
+
+        if (type(name) == (str)) and (1 <= len(name) <= 25):
+            self._name = name
+        else:
+            (print("Name must be string between 1 and 25 characters."))
+
+    name = property(get_name, set_name)
+
+    def get_breed(self):
+        return self._breed
+
+    def set_breed(self, breed):
+
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+            # ipdb.set_trace()
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    breed = property(get_breed, set_breed)
+
+# new_dog = Dog()
+# new_dog.set_breed("Mutt")
